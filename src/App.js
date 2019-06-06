54387af365535fb0deb413f12b60ec8e3d0aac5f
KimberlySwinton <kimberlyswinton@gmail.com>
2019-06-06 00:44:26 -0400

    apps done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+//import todoList constructor from it's relative file
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    tasks: [
+      {
+        task: 'Do Stuff 0',
+        id: 1233,
+        completed: false
+    },
+  
+    {
+        task: 'Do Stuff 1',
+        id: 1231,
+        completed: false
+    },
+  
+    {
+        task: 'Do Stuff 2',
+        id: 1232,
+        completed: false
+    },
+  
+    {
+        task: 'Do Stuff 3',
+        id: 1233,
+        completed: false
+    },
+  
+    {
+        task: 'Do Stuff 4',
+        id: 1233,
+        completed: false
+    }
+    ]
+  }
+   
+  
+
+
+  
   render() {
     return (
       <div className = "App" >
-        <TodoForm />
+        <h2>To Do List</h2>
+      {/* {this.state.tasks.map(taskObj => <p>{taskObj.task}</p>)} */}
+      <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
 }
 
 export default App;
+
+//With props you're passing down your function and things from your state. The data you're using to render in your main application
